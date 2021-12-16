53ed0f07fb7669bd0126f0b3c66ca98cad26b938
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-16 10:29:42 +0000

    Add the views.py code.

--- article/views.py
+++ article/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Blog
 
-# Create your views here.
+class BlogList(generic.ListView):
+    model = Blog
+    queryset = Post.objects.filter(status=1).order_by('article_created_on')
+    template_name = 'article.html'
+    paginate_by = 6
