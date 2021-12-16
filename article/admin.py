f18adff74e32b81da9cd1b018ba9d9a6713090a7
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-16 04:25:40 +0000

    Set up basic admin site and fix minor errors.

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Blog
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'article_created_on')
+    search_fields = ['title', 'article_summery']
+    list_filter = ('status', 'article_created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('article_summery',)
--- article/models.py
+++ article/models.py
@@ -2,20 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Posted"))
 
 
-STATUS =((0, "Draft"), (1, "Posted"))
-
-
-class Article(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="article_posts")
-    article_updated_on = models.TimeField(auto_now=True)
+    updated_on = models.TimeField(auto_now=True)
     article_summery = models.TextField()
     article_image = CloudinaryField('image', default='placeholder')
-    article_video = CloudinaryField('video', default='placeholder')
     article_created_on = models.TimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     article_likes = models.ManyToManyField(User, related_name='article_likes', blank=True)
@@ -33,16 +29,17 @@ class Article(models.Model):
     def article_total_dislikes(self):
         return self.article_dislikes.count()
 
-class Article_comments(models.Model):
-    Article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='article_comments')
+
+class BlogComment(models.Model):
+    Blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='article_comments')
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
-    message_created_on = models.TimeField(auto_now_add=True)
-    article_comment_approval = models.BooleanField(default=False)
+    created_on = models.TimeField(auto_now_add=True)
+    blog_comment_approval = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['-message_created_on']
+        ordering = ['-created_on']
 
     def __str__(self):
-        return f"Article_comment {self.name} {self.message_created_on} {self.message} "
+        return f"Blog_comment {self.name} {self.created_on} {self.message}"
