766bbb9e8117f80ebaf11086d4105edaa16a2d09
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-20 14:41:08 +0000

    Add model HeroImage to home models.py to allow display index.html in browser.

--- home/models.py
+++ home/models.py
@@ -4,3 +4,5 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+class HeroImage(models.Model):
+    home_image = CloudinaryField('image', default='https://res.cloudinary.com/baileym/image/upload/v1638976391/sample.jpg')
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import HeroImage
 
 # Create your views here.
 class MainPage(generic.ListView):
+    model = HeroImage
     template_name = 'index.html'
