f8209948928134277e61bf822b89537ee0c7d13f
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-23 01:30:40 +0000

    Add forms.py to the article folder and try to fix heroimage no success.

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
 
 class HeroImage(models.Model):
     home_image = CloudinaryField('image', default='https://res.cloudinary.com/baileym/image/upload/v1638976391/sample.jpg')
