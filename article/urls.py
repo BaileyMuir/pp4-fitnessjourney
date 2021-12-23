6a0b4019e0922d24df84dc691cc14e84d5a0bde2
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-23 02:20:26 +0000

    Try to make it so the actual article opens in a new page but exsperience 'ValueError at /post1/'.

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("article", views.BlogList.as_view(), name='article')
+    path("article", views.BlogList.as_view(), name='article'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Blog
+from .forms import ArticleCommentForm
 
 class BlogList(generic.ListView):
     model = Blog
     queryset = Blog.objects.filter(status=1).order_by('-article_created_on')
     template_name = 'article.html'
     paginate_by = 6
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Blog.objects.filter(status=1)
+        blog = get_object_or_404(queryset, slug=slug)
+        article_comments = blog.article_comments.filter(blog_comment_approval=True).order_by("-created_on")
+        liked = False
+        if blog.article_likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "blog": Blog,
+                "article_comments": article_comments,
+                "article_Commented": False,
+                "liked": liked,
+                "article_comment_form": ArticleCommentForm()
+            },
+        )
