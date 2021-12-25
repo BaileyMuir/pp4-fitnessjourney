ed158b9e3b3d2afcbbe7454bf1565eab1c17182d
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-25 04:58:07 +0000

    Remove the unused model.py in home and used views.py to render the html page in the web-browser.

--- home/models.py
+++ home/models.py
@@ -2,11 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
-
-
-class MainPageContent(models.Model):
-    home_image = CloudinaryField('image', default='placeholder')
-    article_td = models.TextField()
-    routines_td = models.TextField()
-    recipies_td = models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import generic
-from .models import MainPageContent
 
 # Create your views here.
 
-
-class MainPage(generic.ListView):
-    model = MainPageContent
-    template_name = 'index.html'
+def HomeP(request):
+    return render(request, 'index.html')
