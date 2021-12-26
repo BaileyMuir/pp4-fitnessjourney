6380789d35679f64a16c7dbb39be603e7000ae9e
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-26 00:49:56 +0000

    Set up the fitnessjournal views.py.

--- fitnessjournal/views.py
+++ fitnessjournal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import JournalPost
 
 # Create your views here.
+
+class journalList(generic.ListView):
+    model = JournalPost
+    queryset = JournalPost.objects.filter(status=1).order_by('-created_on')
+    template_name = 'journal.html'
+    paginate_by = 4
