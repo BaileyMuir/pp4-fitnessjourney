842ceb95e933dbb461b2669e56203d5c681cdd2e
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-26 06:00:15 +0000

    got the slug to work.

--- fitnessjournal/urls.py
+++ fitnessjournal/urls.py
@@ -2,5 +2,6 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('journals', views.JournalList.as_view(), name='journals')
+    path('journals', views.JournalList.as_view(), name='journals'),
+    path('<slug:slug>/', views.JournalDetail.as_view(), name='journal_detail'),
 ]
--- fitnessjournal/views.py
+++ fitnessjournal/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import JournalPost
 
 # Create your views here.
@@ -9,3 +9,27 @@ class JournalList(generic.ListView):
     queryset = JournalPost.objects.filter(status=1).order_by('-created_on')
     template_name = 'journal.html'
     paginate_by = 4
+
+class JournalDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = JournalPost.objects.filter(status=1)
+        journalpost = get_object_or_404(queryset, slug=slug)
+        journalcomments = journalpost.journalcomments.filter(message_approved=True).order_by('created_on')
+        # liked = False
+        # if journalpost.journal_likes.filter(id=self.request.user.id).exhists():
+        #     liked = True
+        # disliked = False
+        # if journalpost.journal_dislikes.filter(id=self.request.user.id).exhists():
+        #     disliked = True
+
+        return render(
+            request,
+             "journal_detail.html",
+             {
+                "journalpost": journalpost,
+                "journalcomments": journalcomments,
+                # "liked": liked,
+                # "disliked": disliked,
+             },
+        )
