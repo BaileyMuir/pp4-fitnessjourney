c6e0badc84d10a0e559117d7a9c9c949fb70d2a6
BaileyMuir <BaileyIRMuir@outlook.com>
2021-12-26 09:56:20 +0000

    Trying to set up commenting.

--- fitnessjournal/views.py
+++ fitnessjournal/views.py
@@ -24,12 +24,59 @@ class JournalDetail(View):
         if journalpost.journal_dislikes.filter(id=self.request.user.id).exists():
             disliked = True
 
+        Journal_Comment_Form = JournalCommentForm(data=request.POST)
+
+        if Journal_Comment_Form.is_valid():
+            Journal_Comment_Form.instance.email = request.user.email
+            Journal_Comment_Form.instance.name = request.user.username
+            JournalComment = Journal_Comment_Form.save(commit=False)
+            JournalComment.post = post
+            JournalComment.save()
+        else:
+            Journal_Comment_Form = Journal_Comment_Form
+
         return render(
             request,
              "journal_detail.html",
              {
                 "journalpost": journalpost,
                 "journalcomments": journalcomments,
+                "commented": False,
+                "liked": liked,
+                "disliked": disliked,
+                "Journal_Comment_Form": JournalCommentForm()
+             },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = JournalPost.objects.filter(status=1)
+        journalpost = get_object_or_404(queryset, slug=slug)
+        journalcomments = journalpost.journalcomments.filter(message_approved=True).order_by('created_on')
+        liked = False
+        if journalpost.journal_likes.filter(id=self.request.user.id).exists():
+            liked = True
+        disliked = False
+        if journalpost.journal_dislikes.filter(id=self.request.user.id).exists():
+            disliked = True
+
+        Journal_Comment_Form = JournalCommentForm(data=request.POST)
+
+        if Journal_Comment_Form.is_valid():
+            Journal_Comment_Form.instance.email = request.user.email
+            Journal_Comment_Form.instance.name = request.user.username
+            JournalComment = Journal_Comment_Form.save(commit=False)
+            JournalComment.post = post
+            JournalComment.save()
+        else:
+            Journal_Comment_Form = Journal_Comment_Form
+
+        return render(
+            request,
+             "journal_detail.html",
+             {
+                "journalpost": journalpost,
+                "journalcomments": journalcomments,
+                "commented": True,
                 "liked": liked,
                 "disliked": disliked,
                 "Journal_Comment_Form": JournalCommentForm()
